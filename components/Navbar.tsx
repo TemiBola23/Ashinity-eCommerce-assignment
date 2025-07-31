e352fd4e3f8af373d0fa2401bb22ea093a036b13
Kulogun Temitope <122876739+TemiBola23@users.noreply.github.com>
2025-07-31 04:12:46 +0100

    Create  Navbar.tsx

---
+++ components/Navbar.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+export default function Navbar() {
+  return (
+    <nav className="bg-gray-900 text-white p-4 flex gap-4">
+      <Link href="/">Home</Link>
+      <Link href="/about">About</Link>
+      <Link href="/contact">Contact</Link>
+      <Link href="/shop">Shop</Link>
+      <Link href="/cloth">Cloth</Link>
+    </nav>
+  );
+}
